e30de93288fc4e5c6ad28c2700edd748a31555fc
Prasanna Kumar <prassee@prassee.me>
2019-10-06 22:44:11 +0530

    new module on same level of lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod Fruits;
 mod Storage;
 mod Vegies;
+mod publik;
 
 use Fruits::Mango;
 use Vegies::Onion;
@@ -8,4 +9,5 @@ use Vegies::Onion;
 pub fn mangonion() {
     Mango::ripen();
     Onion::chop();
+    publik::publik_fnc();
 }
---
+++ src/publik.rs
@@ -0,0 +1,3 @@
+pub fn publik_fnc() {
+
+}
