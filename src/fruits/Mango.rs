dc2a39ac7b868c68350683ec377916b9094ef7bb
Prasanna Kumar <prassee@prassee.me>
2019-10-15 19:02:20 +0530

    added module example

--- src/fruits/Mango.rs
+++ src/fruits/Mango.rs
@@ -3,6 +3,10 @@ use crate::publik;
 
 pub fn ripen() {
     publik::publik_fnc();
-    publik::LatLong::apply(23.3, 44.5);
+    let ll = publik::LatLong::apply(23.3, 44.5);
+    let x = publik::LatLong::apply(223.8, 122.3);
+    println!("{:?}", x);
+    ll.print_lat_long();
+    println!("{:?}", ll);
     fridge::store_fruits();
 }
--- src/publik.rs
+++ src/publik.rs
@@ -1,5 +1,6 @@
 pub fn publik_fnc() {}
 
+#[derive(Debug)]
 pub struct LatLong {
     pub lat: f32,
     pub long: f32,
@@ -8,5 +9,8 @@ pub struct LatLong {
 impl LatLong {
     pub fn apply(lat: f32, long: f32) -> LatLong {
         LatLong { lat, long }
+    }
+    pub fn print_lat_long(&self) {
+        println!("{} , {}", self.lat, self.long);
     }
 }
